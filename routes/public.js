915663364c7e23878163ceef1af6e6fe433328b8
YosuaniBG <yborgesg@gmail.com>
2023-06-06 18:35:37 -0700

    Actualizado

--- routes/public.js
+++ routes/public.js
@@ -6,17 +6,17 @@ const router = express.Router();
 router.get("/teachers", teachersAvailables); 
 
 // Rutas para filtros ----------------------------------------------------------------------------------
-// Ruta que ejecuta un filtro para los profesores por materia api/student/teachers/filterBySubject?subject=matematicas
+// Ruta que ejecuta un filtro para los profesores por materia api/public/teachers/filterBySubject?subject=matematicas
 router.get("/teachers/filterBySubject", filterBySubject);
 
-// Ruta que ejecuta un filtro para los profesores por precio api/student/teachers/filterByPrice?min_price=50&max_price=100
+// Ruta que ejecuta un filtro para los profesores por precio api/public/teachers/filterByPrice?min_price=50&max_price=100
 router.get("/teachers/filterByPrice", filterByPrice);
 
-// Ruta que ejecuta un filtro para los profesores por experiencia api/student/teachers/filterByExperience?experience=5
+// Ruta que ejecuta un filtro para los profesores por experiencia api/public/teachers/filterByExperience?experience=5
 router.get("/teachers/filterByExperience", filterByExperience);
 
 // Ruta que ejecuta un filtro combinado para los profesores por materia, precio y experiencia
-// api/student/teachers/filterCombined?teachers?subject=ingles&min_price=50&years_of_experience=3
+// api/public/teachers/filterCombined?teachers?subject=ingles&min_price=50&years_of_experience=3
 router.get("/teachers/filterCombined", filterCombined);
 
 module.exports = router;
