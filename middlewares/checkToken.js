51efb0f3637c839d037c3254a23bb9753c316232
YosuaniBG <yborgesg@gmail.com>
2023-06-06 22:27:40 -0700

    corrigiendo header

--- middlewares/checkToken.js
+++ middlewares/checkToken.js
@@ -4,11 +4,11 @@ const { getUserById } = require('../models/userModel');
 // Middleware para chequear que el ususario que accede tenga un token válido
 const checkToken = async (req, res, next) => {
 
-    if(!req.headers['Authorization']){
+    if(!req.headers['authorization']){
         return res.status(500).json({ msg: 'Debe incluirse la cabecera Authorization'});
     }
 
-    const token = req.headers['Authorization'];
+    const token = req.headers['authorization'];
 
     let obj;
     try{
