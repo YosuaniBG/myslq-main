4df6511afd81ffe5b6072f569f1d6c3ccdff8374
YosuaniBG <yborgesg@gmail.com>
2023-06-11 08:37:03 -0700

    correcion en nombre de la tabla comments

--- models/userModel.js
+++ models/userModel.js
@@ -77,7 +77,7 @@ const getMyStudent = (id_teacher, id_student) => {
 }
 
 const getMyTeacher = (id_student, id_teacher) => {
-    return db.query('SELECT u.*, ts.score, ts.comment FROM users as u JOIN teachers_students as ts ON ts.id_teacher = u.id_user WHERE ts.id_teacher = ? AND ts.id_student = ? AND u.status = 1', [id_teacher, id_student]);
+    return db.query('SELECT u.*, ts.score, ts.comments FROM users as u JOIN teachers_students as ts ON ts.id_teacher = u.id_user WHERE ts.id_teacher = ? AND ts.id_student = ? AND u.status = 1', [id_teacher, id_student]);
 }
 
 const getTeachersAvailables = () => {
